ceddd22f28fe608aa6a0ae6cfbe512254793d6f1
Bryan Yi <bryanyi1206@gmail.com>
2021-02-16 08:54:48 -0800

    using raw SQL queries in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,7 +11,15 @@ class api_one(APIView):
     def get(self, request, *args, **kwargs):
         # querySet = covidVaccinationData.objects.all()
         
-        querySet = covidVaccinationData.objects.raw("SELECT * FROM VaccinationData.WD_covid_vaccination_project_data_1 WHERE country='Andorra'")
+        mySQL = """
+            
+            SELECT *  
+            FROM VaccinationData.WD_covid_vaccination_project_data_1 
+            WHERE country='Andorra'
+            
+            """
+        
+        querySet = covidVaccinationData.objects.raw(mySQL)
     
         serializer = covidVaccinationDataSerializer(querySet, many=True)
         return Response(serializer.data)
