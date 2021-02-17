62f8caa9e224f581f8c112ec6521f28b8fe6f209
Bryan Yi <bryanyi1206@gmail.com>
2021-02-17 12:06:33 -0800

    building out frontend components

--- frontend/src/components/CountryStats.js
+++ frontend/src/components/CountryStats.js
@@ -7,14 +7,20 @@ const CountryStats = () => {
       <div className="total_vaccinations">
         <h3>Total Vaccinations</h3>
         <p>Canada</p>
+        <p>February 6th, 2021</p>
+        <h2>1234</h2>
       </div>
       <div className="daily_vaccinations">
         <h3>Daily Vaccinations</h3>
         <p>Canada</p>
+        <p>February 6th, 2021</p>
+        <h2>1234</h2>
       </div>
       <div className="vaccines">
         <h3>Vaccine</h3>
         <p>Canada</p>
+        <p>February 6th, 2021</p>
+        <h2>1234</h2>
       </div>
     </div>
   );
--- frontend/src/components/InfoDetails.js
+++ frontend/src/components/InfoDetails.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "../css/InfoDetails.css";
 
 const InfoDetails = () => {
-  return <div>InfoDetails component</div>;
+  return (
+    <div className="Details">
+      <div className="sources_block">
+        <h2>Sources</h2>
+        <h3>Canada</h3>
+      </div>
+      <div className="most_vaccinated">
+        <h2>Most Vaccinated</h2>
+        <h3>Israel</h3>
+        <p>Total vaccinations: 1234</p>
+      </div>
+    </div>
+  );
 };
 
 export default InfoDetails;
