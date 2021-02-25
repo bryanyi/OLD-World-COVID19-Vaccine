fbc9981b9300dd09199cbe018c3e1bf376c80f23
Bryan Yi <bryanyi1206@gmail.com>
2021-02-24 16:12:21 -0800

    heroku

--- covid_vaccine_tracker/settings.py
+++ covid_vaccine_tracker/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['world-covid-vaccinations.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['covid19-vaccinations.herokuapp.com','127.0.0.1']
 
 
 # Application definition
